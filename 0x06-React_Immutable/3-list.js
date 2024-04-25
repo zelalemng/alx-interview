88b6b41fa721df270af42d56867cfb3a38dbfef3
zelalemng <zedneg@gmail.com>
2024-04-25 13:22:53 +0000

    task_3

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,9 +1,9 @@
 import { List } from 'immutable';
 
-export default function getListObject (array) {
+export function getListObject (array) {
     return List(array);
 }
 
-export default function addElementToList (list, element) {
+export function addElementToList (list, element) {
     return list.push(element);
 }
